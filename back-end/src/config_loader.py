1eb20e992f2ab3e36cc54975d6bf7727b0c07f57
roeepaz <roeepaz333531@gmail.com>
2025-06-10 15:03:31 +0300

    Fix; chamge the config file to YAML

--- back-end/src/config_loader.py
+++ back-end/src/config_loader.py
@@ -1,13 +1,13 @@
-import json
 import os
+import yaml  # PyYAML
 
-CONFIG_PATH = os.path.join(os.path.dirname(__file__),"app_config.json")
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "app_config.yaml")
 
-def load_config():
-    """Load the JSON config file and return it as a dictionary."""
+def load_config() -> dict:
+    """Load the YAML config file and return it as a dictionary."""
     try:
         with open(CONFIG_PATH, "r") as file:
-            return json.load(file)
+            return yaml.safe_load(file) or {}
     except Exception as e:
         print(f"Error loading config: {e}")
         return {}
@@ -15,6 +15,6 @@ def load_config():
 # Load config at startup
 CONFIG = load_config()
 
-def get_config_value(key, default=None):
+def get_config_value(key: str, default=None):
     """Get a specific value from the config by key."""
     return CONFIG.get(key, default)
--- back-end/src/routes.py
+++ back-end/src/routes.py
@@ -1,11 +1,7 @@
-import json
-import os
 from flask import Flask, request, jsonify, render_template
-
 from api_utils import handle_selected_plots
 from recommendation.recomendations_manager import get_recommendation_base_on_strategy
-
-CONFIG_PATH = os.path.join(os.path.dirname(__file__),"app_config.json")
+from config_loader import load_config
 
 def handle_error(e, status_code=500, error_type=None):
     """Handle an exception and return a standardized JSON error response.
@@ -65,9 +61,7 @@ class Routes:
                 Loads config and event list, then renders the home template.
             """
             try:
-                with open(CONFIG_PATH, "r") as file:
-                    config = json.load(file)
-
+                config = load_config()
                 if self.handler is None:
                     raise RuntimeError("Handler not initialized")
 
@@ -102,8 +96,7 @@ class Routes:
                 Reads the local 'app_config.json' file and returns its content.
             """
             try:
-                with open(CONFIG_PATH, "r") as file:
-                    config = json.load(file)
+                config = load_config()
                 return jsonify({"success": True, "data": config})
             except Exception as e:
                 return handle_error(e)
