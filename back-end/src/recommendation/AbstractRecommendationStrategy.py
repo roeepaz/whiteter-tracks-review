53d78c0fea8099bcfdc39ef44973dab511bd9505
roeepaz <roeepaz333531@gmail.com>
2025-06-11 14:22:06 +0300

    Reafactor: change the RecommendationStrategy class name

---
+++ back-end/src/recommendation/AbstractRecommendationStrategy.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+class AbstractRecommendationStrategy(ABC):
+    """
+    An abstract base class for recommendation strategies.
+    
+    Each subclass must implement the `recommend` method,
+    which returns a list of recommended plots based on the
+    given event ID and selected plots.
+    """
+
+    @abstractmethod
+    def recommend(self, event_id: str, selected_plots: List[dict]) -> List[dict]:
+        """
+        Generate a list of recommended plots.
+
+        Parameters:
+            event_id (str): The ID of the event to base recommendations on.
+            selected_plots (List[dict]): A list of selected plot data.
+
+        Returns:
+            List[dict]: A list of recommended plots.
+        """
+        pass
--- back-end/src/recommendation/base.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class RecommendationStrategy(ABC):
-    @abstractmethod
-    def recommend(self, event_id, selected_plots) -> list[dict]:
-        pass
